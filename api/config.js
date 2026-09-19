71b7c5f88d3bf601a4becbd02fb53bea358009bd
Uusame70 <usameismail70@gmail.com>
2026-09-19 21:49:02 +0300

    Delete api/config.js

--- api/config.js
+++
@@ -1,7 +0,0 @@
-// api/config.js
-// ═══════════════════════════════════════════════════════
-//  TEK YERDEN AYARLAR — buradan değiştir, gerisi otomatik
-// ═══════════════════════════════════════════════════════
-export const TWOCHAT_API_KEY = 'UAK6f703c42-c939-4575-89cc-35922b2faca9';
-export const TWOCHAT_PHONE = '905388444275';
-// ═══════════════════════════════════════════════════════
